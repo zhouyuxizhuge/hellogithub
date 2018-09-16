675006c546a58de74cfa1d8ba94590b6f1f550b4
zhouyuxizhuge <zhouyuxizhuge2018@163.com>
2018-09-16 13:48:40 +0800

    idea初始化

--- src/Hellodemo.java
+++ src/Hellodemo.java
@@ -1,5 +1,7 @@
 /**
- * Created by zhouyu on 2018/9/16.
+ * 功能描述: 
+ * @auther: zhouyu
+ * @date: 2018/9/16 13:46
  */
 public class Hellodemo {
     public static void main(String[] args) {
---
+++ src/TestTmp.java
@@ -0,0 +1,7 @@
+/**
+ * 功能描述: 
+ * @auther: zhouyu
+ * @date: 2018/9/16 13:46
+ */
+public class TestTmp {
+}
